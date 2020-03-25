5011abf03f4968256ad94a8e1058bb899a819a7e
Jacky-Giti <song.fuwei@giti.com>
2020-03-25 16:50:09 +0800

    1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Id4Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
